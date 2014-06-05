afcecdc3164ed642567b3bce1d815a56423a4a83
Logan Saruwatari <lsaruwatari@gmail.com>
2014-06-05 17:11:40 -0600

    added changes to plugin per Jeremy

--- ls-latest-posts/ls-latest-posts.php
+++ ls-latest-posts/ls-latest-posts.php
@@ -6,6 +6,14 @@
  * Author: Logan Saruwatari
  */
 
+//code copied from documentation
+//changes excerpt length from the default 55 to whatever
+function custom_excerpt_length( $length ) {
+    return 6;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+
+
 define( ls_latest_dir, plugin_dir_path( __FILE__ ));
 define( ls_latest_url, plugin_dir_url( __FILE__ ));
 
@@ -13,38 +21,37 @@ define( ls_latest_url, plugin_dir_url( __FILE__ ));
 //beginning of plugin shortcode implementation
 
 
-    function latest_news($amount)
+    function latest_news($atts)
     {   
-    
+        //allows for custom input attribute called amount
+        //default value is 2
+        extract( shortcode_atts(array('amount' => 2), $atts, 'latest_news' ));
+
         echo"<div class='large-4 medium-6 columns' data-equalizer-watch='' style='height: 280px;'>
              <div class='box teaser news'>
              <h4>Latest News</h4>";
-        $args = array('posts_per_page'=>$amount, 'post_type'=>'post', 'orderby'=>'post_date', 'order'=>'DESC');
+        $args = array('posts_per_page'=>$amount , 'post_type'=>'post', 'orderby'=>'post_date', 'order'=>'DESC');
         $posts_array = get_posts($args);
          foreach ($posts_array as $post)
          {
             if(!empty($post))
             {
                 setup_postdata($post);
-                echo "<a class='story clearfix' href='" . get_the_permalink($post) . "'>
-                      <img src='./Steel Panther_files/75x75&text=[News Thumbnail]'>";
+                echo "<a class='story clearfix' href='" . get_the_permalink($post) . "'>";
+                      if(!empty(get_the_post_thumbnail($post->ID)))  
+                           echo get_the_post_thumbnail($post->ID,array(75,75));
+                      else echo "<img src='http://placehold.it/75x75' />";
                 echo "<p><span class='title'>" . get_the_title($post) . "</span>";
-                echo  get_the_content($post) . "</br></p></a>";
+                echo  the_excerpt($post) . "</br></p></a>";
             }
          }
          echo "</div></div>";
     }
-    //function created to pass value by way of hard code
-    function pass_function()
-    {
-        return latest_news(2);
-    }
 
     //add shortcode [news] to call pass_function
-    //couldn't figure out how to pass parameters in this function
     function register_shortcode()
     {
-        add_shortcode( 'news', 'pass_function');
+        add_shortcode( 'news', 'latest_news');
     }
 
     // Include Plugin Styles
